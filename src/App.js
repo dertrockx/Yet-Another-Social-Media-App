ee6dcf7cea82750e546635560232944d16477e63
Ian Salazar <iisalazar@up.edu.ph>
2021-04-03 10:03:06 +0800

    feat: implemented react-toastify

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import { toast, ToastContainer } from "react-toastify";
 import Navbar from "./components/molecules/Navbar";
 import Feed from "./components/templates/Feed";
 import SignUpPage from "./components/pages/SignUp";
+import LoginPage from "./components/pages/Login";
 import PrivateRoute from "./components/specials/PrivateRoute";
 // context
 import AuthState from "./context/auth";
 
 import "./reset.css";
 import "./App.css";
-
+import "react-toastify/dist/ReactToastify.css";
 const App = () => {
 	return (
 		<AuthState>
@@ -19,8 +20,17 @@ const App = () => {
 					<Navbar />
 					<Switch>
 						<Route exact path="/signup" component={SignUpPage} />
+						<Route exact path="/login" component={LoginPage} />
 						<PrivateRoute exact path="/" component={Feed} />
 					</Switch>
+					<ToastContainer
+						position="bottom-center"
+						autoClose={5000}
+						hideProgressBar={false}
+						newestOnTop={false}
+						closeOnClick
+						rtl={false}
+					/>
 				</div>
 			</Router>
 		</AuthState>
