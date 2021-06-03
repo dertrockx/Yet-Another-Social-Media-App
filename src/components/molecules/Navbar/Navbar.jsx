31e9876cab21ea455416af49e2ef1242d97604e9
Ian Salazar <iisalazar@up.edu.ph>
2021-06-03 17:36:31 +0800

    finally got it working with the loadUser feature

--- src/components/molecules/Navbar/Navbar.jsx
+++ src/components/molecules/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 
 import AuthContext from "../../../context/auth/authContext";
 import { NavLink, useHistory } from "react-router-dom";
@@ -6,54 +6,59 @@ import { NavLink, useHistory } from "react-router-dom";
 import "./navbar.css";
 
 const Navbar = () => {
-	const authContext = useContext(AuthContext);
-	const history = useHistory();
-	const { isAuthenticated = false, logout = () => {} } = authContext;
-	const handleLogout = () => {
-		logout();
-		history.push("/login");
-	};
-	return (
-		<nav className="navbar">
-			<div className="left">
-				<span className="logo-text">
-					Social
-					<span className="light">app</span>
-				</span>
-			</div>
-			<div className="right">
-				{isAuthenticated ? (
-					<>
-						<p className="item">Feed</p>
-						<p className="item">Profile</p>
-						<p className="item">Help</p>
-						<p className="item" onClick={handleLogout}>
-							Logout
-						</p>
-					</>
-				) : (
-					<>
-						<NavLink
-							className="item"
-							activeClassName="is-active"
-							to="/login"
-							exact
-						>
-							Login
-						</NavLink>
-						<NavLink
-							className="item"
-							activeClassName="is-active"
-							to="/signup"
-							exact
-						>
-							Signup
-						</NavLink>
-					</>
-				)}
-			</div>
-		</nav>
-	);
+  const authContext = useContext(AuthContext);
+  const history = useHistory();
+  const { isAuthenticated = false, logout = () => {}, loadUser } = authContext;
+  const handleLogout = () => {
+    logout();
+    history.push("/login");
+  };
+
+  useEffect(() => {
+    loadUser();
+    // eslint-disable-next-line
+  }, []);
+  return (
+    <nav className="navbar">
+      <div className="left">
+        <span className="logo-text">
+          Social
+          <span className="light">app</span>
+        </span>
+      </div>
+      <div className="right">
+        {isAuthenticated ? (
+          <>
+            <p className="item">Feed</p>
+            <p className="item">Profile</p>
+            <p className="item">Help</p>
+            <p className="item" onClick={handleLogout}>
+              Logout
+            </p>
+          </>
+        ) : (
+          <>
+            <NavLink
+              className="item"
+              activeClassName="is-active"
+              to="/login"
+              exact
+            >
+              Login
+            </NavLink>
+            <NavLink
+              className="item"
+              activeClassName="is-active"
+              to="/signup"
+              exact
+            >
+              Signup
+            </NavLink>
+          </>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/context/types.js
+++ src/context/types.js
@@ -3,6 +3,7 @@ export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const SIGNUP_ERROR = "SIGNUP_ERROR";
 export const LOGIN_ERROR = "LOGIN_ERROR";
 export const LOGOUT = "LOGOUT";
+export const LOAD_USER = "LOAD_USER";
 
 export const GET_POSTS = "GET_POSTS";
 export const CREATE_POST = "CREATE_POST";
